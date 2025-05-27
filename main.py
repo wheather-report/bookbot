4a74eadbd82913c8f77e5c5ac5d5e37bb14598a4
wheather-report <htmeador@gmail.com>
2025-05-26 20:06:13 -0700

    finished this project, passed all tests

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-#Main
+# Main
+from stats import sort_alpha_chars
 from stats import get_num_words
+from stats import get_character_counts
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    print("============ BOOKBOT ============")
+    book_path = sys.argv[1]
+    print(f"Analyzing book found at {book_path}...")
     text = get_book_text(book_path)
-#    print(text)
+    print("----------- Word Count -----------")
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
-
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    num_char = get_character_counts(text)
+    sort_alpha = sort_alpha_chars(num_char)
+    for char_dict in sort_alpha:
+        print(f"{char_dict['char']}: {char_dict['num']}")   
+        
 def get_book_text(path):
     with open(path) as f:
         return f.read()
--- stats.py
+++ stats.py
@@ -1,3 +1,24 @@
 def get_num_words(book_text):
     words = book_text.split()
     return len(words)
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_alpha_chars(char_dict):
+    alpha_list=[]
+    for char in char_dict:
+        if char.isalpha() is True:
+            alpha_list.append({"char": char, "num": char_dict[char]})
+    alpha_list.sort(reverse=True, key=sort_on)
+    return alpha_list
+
+def get_character_counts(text):
+    char_counts = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in char_counts:
+            char_counts[lowered] += 1
+        else:
+            char_counts[lowered] = 1
+    return char_counts
